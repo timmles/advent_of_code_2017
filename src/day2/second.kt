32b1523539b747bca341524635ebffd68c64084d
Tim Lewis <mail@timlew.is>
2017-12-03 22:45:44 +0200

    day 2 part 2

--- src/day2/second.kt
+++ src/day2/second.kt
@@ -27,3 +27,45 @@ class Second {
         return map
     }
 }
+
+class D2Part2 {
+
+    fun execute(input: Array<String>): Int {
+        val output: List<Int> = input.map { string -> divisible(string) }
+
+        return output.sum()
+    }
+
+    private fun divisible(string: String): Int {
+        val map: List<Int> = transform(string)
+
+        var a: Int = 0
+        var b: Int = 0
+
+        loop@ for (num in map) {
+            for (divisible in map.minusElement(num)) {
+                if (autoMod(divisible, num) == 0) {
+                    a = num
+                    b = divisible
+                    break@loop
+                }
+            }
+        }
+
+        return autoDivide(a, b)
+    }
+
+    private fun autoDivide(a: Int, b: Int): Int {
+        return maxOf(a,b) / minOf(a,b)
+    }
+
+    private fun autoMod(a: Int, b: Int): Int {
+        return maxOf(a,b) % minOf(a,b)
+    }
+
+    private fun transform(string: String): List<Int> {
+        val split: List<String> = string.split(' ')
+        val map: List<Int> = split.map { input -> input.toInt() }
+        return map
+    }
+}
